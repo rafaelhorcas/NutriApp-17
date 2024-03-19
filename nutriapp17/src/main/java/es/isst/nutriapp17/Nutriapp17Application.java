6f85f245c535cc226ea75f90bfd4a927830e472e
rafaelhorcas <rafaelhorcasm@gmail.com>
2024-03-19 22:14:10 +0100

    Esqueleto proyecto Springboot

---
+++ nutriapp17/src/main/java/es/isst/nutriapp17/Nutriapp17Application.java
@@ -0,0 +1,13 @@
+package es.isst.nutriapp17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nutriapp17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nutriapp17Application.class, args);
+	}
+
+}
---
+++ nutriapp17/src/test/java/es/isst/nutriapp17/Nutriapp17ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.isst.nutriapp17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nutriapp17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
