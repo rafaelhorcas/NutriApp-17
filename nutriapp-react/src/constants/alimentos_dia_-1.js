5c4b5319549ee56f819ea916450cc7ddb5b90ae3
AlvRG <162025813+AlvRG@users.noreply.github.com>
2024-04-01 17:42:51 +0200

    Add files via upload
    Son los 4 días anteriores al actual

---
+++ nutriapp-react/src/constants/alimentos_dia_-1.js
@@ -0,0 +1,53 @@
+export const mock_alimentos_dia_anterior1 = {
+    "alimentos_dia": [
+      {
+        "nombre": "Cereales",
+        "kcal_totales": 180,
+        "proteinas_totales": 10,
+        "carbohidratos_totales": 9,
+        "grasas_totales": 18
+      },
+      {
+        "nombre": "Macarrones con tomate",
+        "kcal_totales": 206,
+        "proteinas_totales": 4,
+        "carbohidratos_totales": 45,
+        "grasas_totales": 0.4
+      },
+      {
+        "nombre": "Salmon",
+        "kcal_totales": 170,
+        "proteinas_totales": 31,
+        "carbohidratos_totales": 0,
+        "grasas_totales": 3.6
+      },
+      {
+        "nombre": "Ensalada mixta",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      },
+      {
+        "nombre": "Bocadillo jamon serrano",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      },
+      {
+        "nombre": "Palitos de merluza",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      },
+      {
+        "nombre": "Pera",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      }
+    ]
+  }
---
+++ nutriapp-react/src/constants/alimentos_dia_-2.js
@@ -0,0 +1,53 @@
+export const mock_alimentos_dia_anterior2 = {
+    "alimentos_dia": [
+      {
+        "nombre": "Cereales",
+        "kcal_totales": 180,
+        "proteinas_totales": 10,
+        "carbohidratos_totales": 9,
+        "grasas_totales": 18
+      },
+      {
+        "nombre": "Alcachofas con bacon",
+        "kcal_totales": 206,
+        "proteinas_totales": 4,
+        "carbohidratos_totales": 45,
+        "grasas_totales": 0.4
+      },
+      {
+        "nombre": "Lomo de cerdo",
+        "kcal_totales": 170,
+        "proteinas_totales": 31,
+        "carbohidratos_totales": 0,
+        "grasas_totales": 3.6
+      },
+      {
+        "nombre": "Manzana",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      },
+      {
+        "nombre": "Bocadillo jamon serrano",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      },
+      {
+        "nombre": "Pizza",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      },
+      {
+        "nombre": "Yogur",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      }
+    ]
+  }
