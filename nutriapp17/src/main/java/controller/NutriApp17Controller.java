ee3ae79a523914d0f5749abb015a556362651a5e
Luis_Garcia <luis.garcia.villarabide@alumnos.upm.es>
2024-04-04 15:42:10 +0200

    controller2

---
+++ nutriapp17/src/main/java/controller/NutriApp17Controller.java
@@ -0,0 +1,13 @@
+@Controller
+@RequestMapping
+public class TFGController {
+@GetMapping("/") // filtro: recibir autenticación -vista: lista
+@GetMapping("/login") // filtro: recibir autenticación -vista: lista
+@GetMapping("/crear") // crear un nuevo TFG -vista:formulario
+@PostMapping("/guardar") // guardar un TFG -crea/actualiza -vista:lista
+@GetMapping("/editar/{id}") // editar ese TFG -lee -vista:formulario
+@GetMapping("/eliminar/{id}") // eliminar ese TFG -elimina -vista:lista
+@GetMapping("/lista") // lista de TFGs -lee todos -vista:lista
+@GetMapping("/aceptar/{id}") // aceptar una propuesta TFG -actualiza -vista:lista
+@PostMapping("/upload") // subir la memoria -actualiza -vista:lista
+@GetMapping("/download/{id}") // descargar la memoria -lee -vista:fichero
