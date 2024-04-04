02a6320f0dc14e4b1e11a670f09d3e21ef4d4557
AlejandroRoman01 <alejandro.roman@alumnos.upm.es>
2024-04-04 18:18:15 +0200

    controller 3

--- nutriapp17/src/main/java/controller/NutriApp17Controller.java
+++ nutriapp17/src/main/java/controller/NutriApp17Controller.java
@@ -1,9 +1,44 @@
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
 @Controller
 @RequestMapping
 public class TFGController {
 @GetMapping("/") // filtro: recibir autenticación -vista: lista
 @GetMapping("/login") // filtro: recibir autenticación -vista: lista
 @GetMapping("/crear") // crear un nuevo TFG -vista:formulario
+@GetMapping("/registar")
+public String registar(Map<String, Object> model) {
+Alimento Alimento  = new Alimento ();
+model.put("Alimento ", Alimento );
+model.put("accion", "guardar");
+return VISTA_FORMULARIO;
+}
+@PostMapping("/registrado")
+public Alimento registrado(Alimento alimento) {
+return alimento;
+}
 @PostMapping("/guardar") // guardar un TFG -crea/actualiza -vista:lista
 @GetMapping("/editar/{id}") // editar ese TFG -lee -vista:formulario
 @GetMapping("/eliminar/{id}") // eliminar ese TFG -elimina -vista:lista
--- nutriapp17/src/main/java/es/isst/model/Alimento.java
+++ nutriapp17/src/main/java/es/isst/model/Alimento.java
@@ -1,9 +1,85 @@
+import java.util.Date;
+
 public class  Alimento{
+
 @NotEmpty private String nombre;
 private Int cantidad;
 private Double calorias;
 private Double proteinas;
 private Double carbohidratos;
 private Double grasas;
-private Fecha fecha;
-}
+private Date fecha;
+
+
+    public Alimento(String nombre, double cantidad, double calorias, double carbohidratos, double grasas, Date fechaDeConsumo) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.calorias = calorias;
+        this.carbohidratos = carbohidratos;
+        this.grasas = grasas;
+        this.fecha = fecha;
+    }
+
+    // Métodos para acceder y modificar los atributos
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(double calorias) {
+        this.calorias = calorias;
+    }
+
+    public double getCarbohidratos() {
+        return carbohidratos;
+    }
+
+    public void setCarbohidratos(double carbohidratos) {
+        this.carbohidratos = carbohidratos;
+    }
+
+    public double getGrasas() {
+        return grasas;
+    }
+
+    public void setGrasas(double grasas) {
+        this.grasas = grasas;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    // Método para imprimir la información del alimento
+    @Override
+    public String toString() {
+        return "Alimento{" +
+                "nombre='" + nombre + '\'' +
+                ", cantidad=" + cantidad +
+                ", calorias=" + calorias +
+                ", carbohidratos=" + carbohidratos +
+                ", grasas=" + grasas +
+                ", fechaDeConsumo=" + fechaDeConsumo +
+                '}';
+    }
+
+}  
