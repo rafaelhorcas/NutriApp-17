eb7578eb8ecd8cc5379230b816a92ded362a4bfc
SantiLeardy4 <165022224+SantiLeardy4@users.noreply.github.com>
2024-04-05 12:59:46 +0200

    Create ComidaDAO.js

---
+++ nutriapp-react/src/components/mains/ComidaDAO.js
@@ -0,0 +1,9 @@
+public interface FoodDAO {
+    void addFood(Food food);
+    void updateFood(Food food);
+    void deleteFood(int foodId);
+    Food getFoodById(int foodId);
+    List<Food> getAllFoods();
+    List<Food> getFoodsByName(String name);
+    List<Food> getFoodsByCategory(String category);
+}
