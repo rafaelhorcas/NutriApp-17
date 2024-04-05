b8d4a1b5a16831d8142d3476b0f834782cfcc7f3
SantiLeardy4 <165022224+SantiLeardy4@users.noreply.github.com>
2024-04-05 12:58:57 +0200

    Update ComidaDAO.js

--- nutriapp-react/src/components/ComidaDAO.js
+++ nutriapp-react/src/components/ComidaDAO.js
@@ -1,11 +1,1 @@
-import java.util.List;
 
-public interface FoodDAO {
-    void addFood(Food food);
-    void updateFood(Food food);
-    void deleteFood(int foodId);
-    Food getFoodById(int foodId);
-    List<Food> getAllFoods();
-    List<Food> getFoodsByName(String name);
-    List<Food> getFoodsByCategory(String category);
-}
