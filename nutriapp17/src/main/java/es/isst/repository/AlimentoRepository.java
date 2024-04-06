c8410d3b39d4c81e23c49c3abd597f1a6007e402
SantiLeardy4 <165022224+SantiLeardy4@users.noreply.github.com>
2024-04-06 17:08:44 +0200

    Update and rename ComidaRepository.java to AlimentoRepository.java

---
+++ nutriapp17/src/main/java/es/isst/repository/AlimentoRepository.java
@@ -0,0 +1,3 @@
+public interface AlimentoRepository extends CrudRepository<Alimento, String> {
+List<Alimento> findByAlimento(String alimento);
+}
--- nutriapp17/src/main/java/es/isst/repository/ComidaRepository.java
+++
@@ -1,3 +0,0 @@
-public interface ComidaRepository extends CrudRepository<Comida, String> {
-List<Comida> findByComida(String comida);
-}
