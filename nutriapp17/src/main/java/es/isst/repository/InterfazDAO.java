265cb2cd9f38c83256aec70399591e8e13be5b15
SantiLeardy4 <165022224+SantiLeardy4@users.noreply.github.com>
2024-04-06 17:06:56 +0200

    Delete nutriapp17/src/main/java/es/isst/repository/InterfazDAO.java

--- nutriapp17/src/main/java/es/isst/repository/InterfazDAO.java
+++
@@ -1,7 +0,0 @@
-public interface FoodDAO {
-    void addFood(Food food);
-    void updateFood(Food food);
-    void deleteFood(int foodId);
-    Food getFoodById(int foodId);
-    List<Food> getAllFoods();
-}
