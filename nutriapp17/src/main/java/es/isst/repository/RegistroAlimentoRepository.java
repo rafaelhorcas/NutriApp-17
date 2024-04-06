89b16fa24b555daf32dae26590369617af66a8bc
SantiLeardy4 <165022224+SantiLeardy4@users.noreply.github.com>
2024-04-06 17:18:03 +0200

    Update RegistroAlimentoRepository.java

--- nutriapp17/src/main/java/es/isst/repository/RegistroAlimentoRepository.java
+++ nutriapp17/src/main/java/es/isst/repository/RegistroAlimentoRepository.java
@@ -1,3 +1,3 @@
 public interface RegistroAlimentoRepository extends CrudRepository<RegistroAlimento, String> {
-List<RegistroAlimento> findByRegistroAlimento(Int ID);
+List<RegistroAlimento> findByRegistroAlimento(Int id);
 }
