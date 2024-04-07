9e938068a00dea228f6c374402a70643736b395f
MigueeLobo19 <106494438+MigueeLobo19@users.noreply.github.com>
2024-04-07 19:33:41 +0200

    Update alimentos_dia_-6.js

--- nutriapp-react/src/constants/alimentos_dia_-6.js
+++ nutriapp-react/src/constants/alimentos_dia_-6.js
@@ -1,1 +1,53 @@
-
+export const mock_alimentos_dia_anterior6 = {
+    "alimentos_dia": [
+      {
+        "nombre": "Cereales",
+        "kcal_totales": 180,
+        "proteinas_totales": 10,
+        "carbohidratos_totales": 9,
+        "grasas_totales": 18
+      },
+      {
+        "nombre": "Pure de verduras",
+        "kcal_totales": 206,
+        "proteinas_totales": 4,
+        "carbohidratos_totales": 45,
+        "grasas_totales": 0.4
+      },
+      {
+        "nombre": "Hambutguesa de vaca",
+        "kcal_totales": 170,
+        "proteinas_totales": 31,
+        "carbohidratos_totales": 0,
+        "grasas_totales": 3.6
+      },
+      {
+        "nombre": "Ensalada mixta",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      },
+      {
+        "nombre": "Bocadillo jamon serrano",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      },
+      {
+        "nombre": "Filete de atun",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      },
+      {
+        "nombre": "Yogur",
+        "kcal_totales": 90,
+        "proteinas_totales": 3,
+        "carbohidratos_totales": 8,
+        "grasas_totales": 5
+      }
+    ]
+  }
